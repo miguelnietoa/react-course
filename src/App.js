ae726d814213e400481a40ac3e9b9adbe61ea575
Miguel Nieto A <39246879+miguelnietoa@users.noreply.github.com>
2022-01-14 16:16:42 -0500

    Project setup (#1)
    * Initialize project using Create React App
    
    * Add font and images
    
    * Add config files for API and helpers
    
    * Add Global Styles
    
    * Update README with the course info

---
+++ src/App.js
@@ -0,0 +1,13 @@
+// Styles
+import { GlobalStyle } from './GlobalStyle';
+
+function App() {
+  return (
+    <div className="App">
+      Start here.
+      <GlobalStyle />
+    </div>
+  );
+}
+
+export default App;
