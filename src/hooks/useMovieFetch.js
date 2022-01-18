e8aad3fcb1c5e3419e632025ba0b87bb8d219724
Miguel Nieto A <anietom@uninorte.edu.co>
2022-01-18 17:38:20 -0500

    Persist data for a single movie in sessionStorage

--- src/hooks/useMovieFetch.js
+++ src/hooks/useMovieFetch.js
@@ -1,5 +1,7 @@
 import { useState, useEffect } from 'react';
 import API from '../API';
+// Helpers
+import { isPersistedState } from '../helpers';
 
 // eslint-disable-next-line import/prefer-default-export
 export const useMovieFetch = movieId => {
@@ -32,7 +34,18 @@ export const useMovieFetch = movieId => {
       }
     };
 
+    const sessionState = isPersistedState(movieId);
+    if (sessionState) {
+      setState(sessionState);
+      setLoading(false);
+      return;
+    }
     fetchMovie();
   }, [movieId]);
+
+  // Write to sessionStorage
+  useEffect(() => {
+    sessionStorage.setItem(movieId, JSON.stringify(state));
+  }, [movieId, state]);
   return { state, loading, error };
 };
