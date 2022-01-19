7a90583eb62ba6a6512d27354f02412140be810f
Miguel Nieto A <anietom@uninorte.edu.co>
2022-01-19 16:08:37 -0500

    Setup and start to migrate

--- src/index.tsx
+++ src/index.tsx
@@ -1,4 +1,3 @@
-/* eslint-disable react/jsx-filename-extension */
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
@@ -7,5 +6,5 @@ ReactDOM.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
-  document.getElementById('root'),
+  document.getElementById('root')
 );
