feaa52e486187d9eff498649249f96861547143b
Miguel Nieto A <anietom@uninorte.edu.co>
2022-01-19 21:06:44 -0500

    Fix initial state in useHomeFetch

--- src/hooks/useHomeFetch.js
+++ src/hooks/useHomeFetch.js
@@ -15,8 +15,8 @@ const initialState = {
 export const useHomeFetch = () => {
   const [searchTerm, setSearchTerm] = useState('');
   const [state, setState] = useState(initialState);
-  const [loading, setLoading] = useState();
-  const [error, setError] = useState();
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(false);
   const [isLoadingMore, setIsLoadingMore] = useState(false);
 
   const fetchMovies = async (page, searchTerm_ = '') => {
